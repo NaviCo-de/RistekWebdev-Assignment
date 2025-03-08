b4120a17498a5b5791842149da7a1b2c3a705081
NaviCo-de <aridarrell30@gmail.com>
2025-03-09 00:00:29 +0700

    Level 2 lagi error di delete dan edit sisa aman, besok otw level 3 semoga bisa

--- Tugas Ristek/TugasRistek/app_utama/views.py
+++ Tugas Ristek/TugasRistek/app_utama/views.py
@@ -34,7 +34,8 @@ def add_data(request):
             'id': new_id,
             "judul_tryout": judul_tryout,
             "kategori": kategori,
-            "tanggal_dibuat": tanggal_dibuat
+            "tanggal_dibuat": tanggal_dibuat,
+            "list_pertanyaan": []
         }
 
         data.append(upload_data)
@@ -43,6 +44,13 @@ def add_data(request):
 
         return redirect('tryout')
     return render(request, 'add_data.html')
+
+def detail_data(request, id, judul_tryout):
+    data = read_json()
+
+    item = next((item for item in data if item['id']==id), None)
+    
+    return render(request, 'detail_data.html', {'item':item})
 
 def edit_data(request, id):
     data = read_json()
@@ -70,3 +78,68 @@ def delete_data(request, id):
     write_json(data)
 
     return redirect('tryout')
+
+def add_question(request, id, judul_tryout):
+    if request.method == 'POST':
+        get_question = request.POST.get('pertanyaan')
+        get_jawaban_salah = request.POST.get('jawaban_salah')
+        get_jawaban_benar = request.POST.get('jawaban_benar')
+
+        data = read_json()
+        for item in data:
+            if item["id"] == id:
+                tryout = item
+                break
+        nomor = len(tryout["list_pertanyaan"]) + 1
+
+        data_pertanyaan = {
+            "nomor_pertanyaan" : nomor,
+            "pertanyaan": get_question,
+            "jawaban_salah": get_jawaban_salah,
+            "jawaban_benar": get_jawaban_benar
+        }
+
+        tryout["list_pertanyaan"].append(data_pertanyaan)
+
+        write_json(data)
+        return redirect('detail_data', judul_tryout=judul_tryout, id = id)
+        
+    return render(request, 'add_question.html')
+
+def edit_question(request, id, judul_tryout, nomor_pertanyaan):
+    data = read_json()
+
+    if request.method == 'POST':
+        for item in data:
+            if item["id"] == id:
+                tryout = item
+                break
+        for pertanyaan in tryout["list_pertanyaan"]:
+            if pertanyaan["nomor_pertanyaan"] == nomor_pertanyaan:
+                entry = pertanyaan
+
+        entry["pertanyaan"] = request.POST.get('pertanyaan_baru')
+        entry["jawaban_benar"] = request.POST.get('jawaban_benar_baru')
+        entry["jawaban_salah"] = request.POST.get('jawaban_salah_baru')
+
+        write_json(data)
+
+        return redirect('detail_data', judul_tryout=judul_tryout, id = id)
+    return render(request, 'edit_question.html')
+
+def delete_question(request, id, judul_tryout, nomor_pertanyaan):
+    data = read_json()
+    
+    for item in data:
+        if item["id"] == id:
+            tryout = item
+            break
+    data = [pertanyaan for pertanyaan in tryout["list_pertanyaan"] if pertanyaan["nomor_pertanyaan"] != nomor_pertanyaan]
+
+    for nomor, questions in enumerate(tryout["list_pertanyaan"], start=1):
+        questions['nomor_pertanyaan'] = nomor
+    
+    write_json(data)
+
+    return redirect('detail_data', judul_tryout=judul_tryout, id = id)
+
